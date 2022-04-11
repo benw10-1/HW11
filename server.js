ea9cb9e6acb8c21fde6ff97a4b602264300c0a33
Ben Wirth <benjaminwirth10@gmail.com>
2022-04-11 10:51:56 -0700

    finished

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const router = require("./routes/api/api")
+const router = require("./routes/api")
 app.use("/api", router)
-
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.get("/notes", async (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
+app.use(express.static(path.join(__dirname, 'public')))
+app.get("*", async (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
